e8a39e1c715027ec410b41cc240c62f774c0f567
Sachin Devdhar <sachdevdhar@gmail.com>
2022-04-17 18:05:07 -0400

    completed all functionalities

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+const jwt = require("jsonwebtoken");
 var app = express();
 var bodyParser = require("body-parser"); // pull information from HTML POST (express4)
 const exphbs = require("express-handlebars");
@@ -6,7 +7,6 @@ var path = require("path"); // Using Path
 app.use(express.static(path.join(__dirname, "public"))); // Connecting to the segments
 app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs"); //Able to render hbs file
-
 app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
@@ -24,7 +24,7 @@ const {
 
 app.get("/getrestaurants", function (req, res) {
   Rest.find(function (err, rest) {
-    if (err) res.send(err);
+    if (err) res.status(404).send(err);
 
     res.json(rest);
   });
@@ -40,11 +40,9 @@ app.get("/api/restaurants", async (req, res) => {
   var perPage = req.query.perPage;
   var borough = req.query.borough;
 
-  console.log(page, perPage, borough);
-
   let temp = await db.getAllRestaurants(page, perPage, borough);
   //console.log("here ");
-  res.send(temp);
+  temp ? res.send(temp) : res.status(404).send("Error fetching data!");
 });
 
 //route for step3 hbs
@@ -148,6 +146,103 @@ app.get("/deleteRestaurant", (req, res) => {
 	ID: <input type="text" name="_id"><br><br>  
 	  <input type="submit" value="Submit">
 	  </form>`);
+});
+
+let databse = [
+  {
+    name: "gfg",
+    work: "knowledge provider",
+    password: "abc",
+  },
+  {
+    name: "suryapratap",
+    work: "technical content writer",
+    password: "123",
+  },
+];
+
+// A demo get route
+app.get("/", (req, res) => {
+  res.json({
+    route: "/",
+    authentication: false,
+  });
+});
+
+// Login route
+app.post("/login", (req, res) => {
+  // Get the name to the json body data
+  const name = req.body.name;
+
+  // Get the password to the json body data
+  const password = req.body.password;
+
+  // Make two variable for further use
+  let isPresent = false;
+  let isPresnetIndex = null;
+
+  // iterate a loop to the data items and
+  // check what data are matched.
+  for (let i = 0; i < databse.length; i++) {
+    // If data name are matched so check
+    // the password are correct or not
+    if (databse[i].name === name && databse[i].password === password) {
+      // If both are correct so make
+      // isPresent variable true
+      isPresent = true;
+
+      // And store the data index
+      isPresnetIndex = i;
+
+      // Break the loop after matching successfully
+      break;
+    }
+  }
+
+  // If isPresent is true, then create a
+  // token and pass to the response
+  if (isPresent) {
+    // The jwt.sign method are used
+    // to create token
+    const token = jwt.sign(databse[isPresnetIndex], "secret");
+
+    // Pass the data or token in response
+    res.json({
+      login: true,
+      token: token,
+      data: databse[isPresnetIndex],
+    });
+  } else {
+    // If isPresent is false return the error
+    res.json({
+      login: false,
+      error: "please check name and password.",
+    });
+  }
+});
+
+// Verify route
+app.get("/auth", (req, res) => {
+  // Get token value to the json body
+  const token = req.body.token;
+
+  // If the token is present
+  if (token) {
+    // Verify the token using jwt.verify method
+    const decode = jwt.verify(token, "secret");
+
+    //  Return response with decode data
+    res.json({
+      login: true,
+      data: decode,
+    });
+  } else {
+    // Return response with error
+    res.json({
+      login: false,
+      data: "error",
+    });
+  }
 });
 
 app.listen(port);
