8afc32c2570b755d7d5373cefb85d4b0160ba800
Sachin Devdhar <sachdevdhar@gmail.com>
2022-04-17 20:14:45 -0400

    final commit

--- app.js
+++ app.js
@@ -1,80 +1,37 @@
 var express = require("express");
 const jwt = require("jsonwebtoken");
-var app = express();
 var bodyParser = require("body-parser"); // pull information from HTML POST (express4)
+const { celebrate, Joi, Segments } = require("celebrate");
 const exphbs = require("express-handlebars");
 var path = require("path"); // Using Path
+var app = express();
 app.use(express.static(path.join(__dirname, "public"))); // Connecting to the segments
 app.engine(".hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs"); //Able to render hbs file
 app.use(bodyParser.urlencoded({ extended: "true" })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: "application/vnd.api+json" })); // parse application/vnd.api+json as json
-var db = require("./db/methods");
+
 var port = process.env.PORT || 8000;
 
-db.initialize();
-
-var Rest = require("./models/restaurants");
+var db = require("./db/methods");
 const {
   getRestaurantById,
   updateRestaurantById,
   deleteRestaurantById,
 } = require("./db/methods");
 
-app.get("/getrestaurants", function (req, res) {
-  Rest.find(function (err, rest) {
-    if (err) res.status(404).send(err);
+//STEP 2: Calling the database connection method
+db.initialize();
 
-    res.json(rest);
-  });
+app.get("/", (req, res) => {
+  res.send("Welcome to the project of Web Framework 1 Course!!");
 });
 
-app.get("/step3", (req, res) => {
-  res.render("./pages/step3", { layout: false });
-});
-
-//route for paginaton through URL
-app.get("/api/restaurants", async (req, res) => {
-  var page = req.query.page;
-  var perPage = req.query.perPage;
-  var borough = req.query.borough;
-
-  let temp = await db.getAllRestaurants(page, perPage, borough);
-  //console.log("here ");
-  temp ? res.send(temp) : res.status(404).send("Error fetching data!");
-});
-
-//route for step3 hbs
-app.post("/api/restaurants", async (req, res) => {
-  var page = req.body.page;
-  var perPage = req.body.perPage;
-  var borough = req.body.Borough;
-
-  console.log(page, perPage, borough);
-
-  let temp = await db.getAllRestaurants(page, perPage, borough);
-  //console.log("here ");
-  res.send(temp);
-});
-
-app.get("/api/restaurants/:_id", async (req, res) => {
-  let id = req.params._id;
-  let result = await getRestaurantById(id);
-  console.log(result);
-  res.send(result);
-});
-
-app.get("/addForm", (req, res) => {
-  res.render("./pages/addform", { layout: false });
-});
-
-// create book and send back all books after creation
+//STEP 2: Add new restaurant by calling db.addNewRestaurant(updatedData) method
 app.post("/api/restaurants", async function (req, res) {
-  // create mongose method to create a new record into collection
-
   var data = req.body;
-  console.log(data);
+
   let updatedData = {
     address: {
       building: data.Building,
@@ -94,17 +51,44 @@ app.post("/api/restaurants", async function (req, res) {
     name: data.Name,
     restaurant_id: data.Restaurant_id,
   };
-  console.log(updatedData);
 
   let result = await db.addNewRestaurant(updatedData);
+  console.log(result);
 
-  res.send(result);
+  result ? res.send(result) : res.status(404).send("Error inserting data!!");
 });
 
-// create book and send back all books after creation
-app.put("/api/restaurants", async (req, res) => {
-  // create mongose method to update an existing record into collection
+//STEP 2: Call db.getAllRestaurants(page, perPage, borough) to use route for paginaton through URL
+app.get(
+  "/api/restaurants",
+  celebrate({
+    [Segments.QUERY]: {
+      page: Joi.number().required(),
+      perPage: Joi.number().required(),
+      borough: Joi.string().required(),
+    },
+  }),
+  async (req, res) => {
+    var page = req.query.page;
+    var perPage = req.query.perPage;
+    var borough = req.query.borough;
 
+    let temp = await db.getAllRestaurants(page, perPage, borough);
+
+    temp ? res.send(temp) : res.status(404).send("Error fetching data!");
+  }
+);
+
+//STEP 2: Call getRestaurantById(id) to get restaurants by id
+app.get("/api/restaurants/:_id", async (req, res) => {
+  let id = req.params._id;
+  let result = await getRestaurantById(id);
+
+  result ? res.send(result) : res.status(404).send("Error getting data!!");
+});
+
+//STEP 2: Call updateRestaurantById(data, id) to update restaurants data by id
+app.put("/api/restaurants", async (req, res) => {
   let id = req.body._id;
   var data = {
     borough: req.body.Borough,
@@ -113,51 +97,50 @@ app.put("/api/restaurants", async (req, res) => {
   };
 
   let result = await updateRestaurantById(data, id);
-  res.send(result);
-  // save the user
+
+  result ? res.send(result) : res.status(404).send("Error updating data!!");
 });
 
-app.get("/addUpdateRestaurant", (req, res) => {
-  res.send(`<form action="/update" method="post">
-  
-	 
-	ID: <input type="text" name="_id"><br><br>
-	  Borough: <input type="text" name="Borough"><br><br>
-	  Cuisine: <input type="text" name="Cuisine"><br><br><br>  
-	  Name: <input type="text" name="Name"><br><br>
-
-  
-	  <input type="submit" value="Submit">
-	  </form>`);
-});
-
-// delete a employee by id
+//STEP 2: Call deleteRestaurantById(id) to delete a restaurant by id
 app.delete("/api/restaurants", async (req, res) => {
   let id = req.body._id;
   let result = await deleteRestaurantById(id);
-  if (result) res.send(result);
-  else res.status(404).send("Error deleting record.");
+
+  result ? res.send(result) : res.status(404).send("Error deleting data!!");
 });
 
-app.get("/deleteRestaurant", (req, res) => {
-  res.send(`<form action="/delete" method="post">
-  
-	 
-	ID: <input type="text" name="_id"><br><br>  
-	  <input type="submit" value="Submit">
-	  </form>`);
+//STEP 3: Access the pagination route through form using .hbs file
+app.get("/step3", (req, res) => {
+  res.render("./pages/step3", { title: "Pagination", layout: false });
 });
 
+//STEP 3: Fetching the values from step 3 form to demo pagination
+app.post("/api/restaurantsForm", async (req, res) => {
+  var page = req.body.page;
+  var perPage = req.body.perPage;
+  var borough = req.body.Borough;
+
+  let temp = await db.getAllRestaurants(page, perPage, borough);
+  temp
+    ? res.send(temp)
+    : res.status(404).send("Error fetching data by pagination!!");
+});
+
+app.get("/addForm", (req, res) => {
+  res.render("./pages/addform", { title: "Add Form", layout: false });
+});
+
+//Demo JWT token accessing to ensure security of our app
 let databse = [
   {
-    name: "gfg",
-    work: "knowledge provider",
-    password: "abc",
+    name: "sachin",
+    work: "Java Developer",
+    password: "sachin1234",
   },
   {
-    name: "suryapratap",
-    work: "technical content writer",
-    password: "123",
+    name: "harshit",
+    work: "Web Developer",
+    password: "harshit1234",
   },
 ];
 
@@ -181,14 +164,11 @@ app.post("/login", (req, res) => {
   let isPresent = false;
   let isPresnetIndex = null;
 
-  // iterate a loop to the data items and
-  // check what data are matched.
+  // iterate a loop to the data items and check what data are matched.
   for (let i = 0; i < databse.length; i++) {
-    // If data name are matched so check
-    // the password are correct or not
+    // If data name are matched so check the password are correct or not
     if (databse[i].name === name && databse[i].password === password) {
-      // If both are correct so make
-      // isPresent variable true
+      // If both are correct so make isPresent variable true
       isPresent = true;
 
       // And store the data index
@@ -199,11 +179,9 @@ app.post("/login", (req, res) => {
     }
   }
 
-  // If isPresent is true, then create a
-  // token and pass to the response
+  // If isPresent is true, then create a token and pass to the response
   if (isPresent) {
-    // The jwt.sign method are used
-    // to create token
+    // The jwt.sign method is used to create token
     const token = jwt.sign(databse[isPresnetIndex], "secret");
 
     // Pass the data or token in response
@@ -231,7 +209,7 @@ app.get("/auth", (req, res) => {
     // Verify the token using jwt.verify method
     const decode = jwt.verify(token, "secret");
 
-    //  Return response with decode data
+    //Return response with decode data
     res.json({
       login: true,
       data: decode,
--- models/restaurants.js
+++ models/restaurants.js
@@ -19,53 +19,5 @@ restaurantSchema = new Schema({
   ],
   name: String,
   restaurant_id: String,
-
-  // "address": {
-  //       "building": {
-  //         "type": "String"
-  //       },
-  //       "coord": {
-  //         "type": [
-  //           "Mixed"
-  //         ]
-  //       },
-  //       "street": {
-  //         "type": "String"
-  //       },
-  //       "zipcode": {
-  //         "type": "String"
-  //       }
-  //     },
-  //     "borough": {
-  //       "type": "String"
-  //     },
-  //     "cuisine": {
-  //       "type": "String"
-  //     },
-  //     "grades": [{
-  //       "date": "Date",
-  //       "grade": "String",
-  //       "score": "Number"
-  //   }],
-  //     // "grades": {
-  //     //   "type": [
-  //     //     "Object",
-  //     //   ],
-  //     //   "date":{
-  //     //     "type": "Date"
-  //     //   },
-  //     //   "grade":{
-  //     //     "type": "String"
-  //     //   },
-  //     //   "score":{
-  //     //     "type": "Number"
-  //     //   },
-  //     // },
-  //     "name": {
-  //       "type": "String"
-  //     },
-  //     "restaurant_id": {
-  //       "type": "String"
-  //     },
 });
 module.exports = mongoose.model("restaurants", restaurantSchema);
